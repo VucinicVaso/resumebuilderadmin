167eba45189edbfcbdaa01a68f61a152ca483b07
vaso.vucinic <vasoct@gmail.com>
2025-07-03 14:44:45 +0200

    updated controller

--- lib/core/translation/impl/wtoolbox_me_mne_translations.dart
+++ lib/core/translation/impl/wtoolbox_me_mne_translations.dart
@@ -58,7 +58,6 @@ class WTMeMneTranslations extends WTTranslations {
       "loginOptionFingerprint": "Otisak prsta",
       "loginOptionFace": "Lice",
       "biometricsMessage": "Molimo potvrdite autentičnost da biste pristupili aplikaciji.",
-      "testText": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       "uploadFile": "Dodaj fajlove",
       "chooseFile": "Odaberite fajlove za otpremanje",
       "selectedFiles": "Odabrani fajlovi",

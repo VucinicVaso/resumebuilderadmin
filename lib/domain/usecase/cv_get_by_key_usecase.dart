c67ef03efe2d86ea6d1ba817963eb0d5afe4bb54
vaso.vucinic <vasoct@gmail.com>
2025-08-26 15:20:52 +0200

    usecases for cv section

---
+++ lib/domain/usecase/cv_get_by_key_usecase.dart
@@ -0,0 +1,28 @@
+import 'package:wtoolboxweb/external/lib_getx.dart';
+import 'package:wtoolboxweb/clean_architecture/usecase/wtw_usecase_with_params.dart';
+import '../entity/cv/cv.dart';
+import '../repository/cv/cv_repository.dart';
+
+class CVGetByKeyUseCase extends WTWUseCaseWithParams<CV, CVGetByKeyUseCaseParams> {
+
+  @override
+  Future<CV> call(CVGetByKeyUseCaseParams? params) async {
+    try {
+      CV? response = await Get.find<CVRepository>().getByKey(params!.key!);
+      return response!;
+    } catch(e) {
+      throw Exception(e);
+    }
+  }
+
+}
+
+class CVGetByKeyUseCaseParams {
+
+  int? key;
+
+  CVGetByKeyUseCaseParams({
+    this.key,
+  });
+
+}

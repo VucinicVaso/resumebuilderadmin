a98cd8062d365f900733b927b84a906caa890202
vaso.vucinic <vasoct@gmail.com>
2025-08-26 14:50:38 +0200

    created usecases for experience section

---
+++ lib/domain/usecase/experience_get_by_key_usecase.dart
@@ -0,0 +1,28 @@
+import 'package:wtoolboxweb/external/lib_getx.dart';
+import 'package:wtoolboxweb/clean_architecture/usecase/wtw_usecase_with_params.dart';
+import '../entity/experience/experience.dart';
+import '../repository/experience/experience_repository.dart';
+
+class ExperienceGetByKeyUseCase extends WTWUseCaseWithParams<Experience, ExperienceGetByKeyUseCaseParams> {
+
+  @override
+  Future<Experience> call(ExperienceGetByKeyUseCaseParams? params) async {
+    try {
+      Experience? response = await Get.find<ExperienceRepository>().getByKey(params!.key!);
+      return response!;
+    } catch(e) {
+      throw Exception(e);
+    }
+  }
+
+}
+
+class ExperienceGetByKeyUseCaseParams {
+
+  int? key;
+
+  ExperienceGetByKeyUseCaseParams({
+    this.key,
+  });
+
+}
---
+++ lib/domain/usecase/experience_insert_usecase.dart
@@ -0,0 +1,28 @@
+import 'package:wtoolboxweb/external/lib_getx.dart';
+import 'package:wtoolboxweb/clean_architecture/usecase/wtw_usecase_with_params.dart';
+import '../entity/experience/experience.dart';
+import '../repository/experience/experience_repository.dart';
+
+class ExperienceInsertUseCase extends WTWUseCaseWithParams<bool, ExperienceInsertUseCaseParams> {
+
+  @override
+  Future<bool> call(ExperienceInsertUseCaseParams? params) async {
+    try {
+      bool? response = await Get.find<ExperienceRepository>().insert(params!.entity!);
+      return response!;
+    } catch(e) {
+      throw Exception(e);
+    }
+  }
+
+}
+
+class ExperienceInsertUseCaseParams {
+
+  Experience? entity;
+
+  ExperienceInsertUseCaseParams({
+    this.entity,
+  });
+
+}
